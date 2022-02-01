2098e698eedf7feda3332033788a1c5249f5153a
JoseMMorales <josemulamorales33@hotmail.com>
2022-02-01 12:01:52 +0100

    Implementing home component to refactory code including filter data in store service

--- src/app/common/store.service.ts
+++ src/app/common/store.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable, Subject } from "rxjs";
+import { BehaviorSubject, Observable } from "rxjs";
+import { map, tap } from "rxjs/operators";
 import { Course } from "../model/course";
+import { createHttpObservable } from "./util";
 
 @Injectable({
   providedIn: "root",
@@ -9,4 +11,25 @@ export class Store {
   private subject = new BehaviorSubject<Course[]>([]);
 
   courses$: Observable<Course[]> = this.subject.asObservable();
+
+  init() {
+    const http$ = createHttpObservable("/api/courses");
+
+    http$
+      .pipe(
+        tap(() => console.log("HTTP request executed")),
+        map((res) => Object.values(res["payload"]))
+      )
+      .subscribe((courses: Course[]) => this.subject.next(courses));
+  }
+
+  selectCourses(category: string) {
+    return this.filterByCategory(category);
+  }
+
+  filterByCategory(category: string) {
+    return this.courses$.pipe(
+      map((courses) => courses.filter((course) => course.category == category))
+    );
+  }
 }
